b6ed9f7eb3b4f4ca5d95130f0cac6ea849a9ec5c
Vincent Klaiber <vincentklaiber@gmail.com>
2015-01-25 10:21:40 +0100

    Move gems into its own file.

---
+++ gems.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Make sure we’re using the latest gems
+sudo gem update --system
+sudo gem update
+
+# Install useful binaries
+sudo gem install sass
+sudo gem install bundler
+
