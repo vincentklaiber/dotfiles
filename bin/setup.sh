d3c9cd3f01bf7165e0459aaa0e1b4fce2f27f2b4
Vincent Klaiber <vincentklaiber@gmail.com>
2015-09-28 09:29:48 +0200

    Fix for CI

--- bin/setup.sh
+++ bin/setup.sh
@@ -221,8 +221,12 @@ logn "Installing PHP:"
 brew install homebrew/php/php70
 sed -i".bak" "s/^\;phar.readonly.*$/phar.readonly = Off/g" /usr/local/etc/php/7.0/php.ini
 sed -i "s/memory_limit = .*/memory_limit = -1/" /usr/local/etc/php/7.0/php.ini
-brew install homebrew/php/composer
-brew install homebrew/php/php-cs-fixer
+if [ -z "$STRAP_CI" ]; then
+  brew install homebrew/php/composer
+  brew install homebrew/php/php-cs-fixer
+else
+  echo "Skipping installing composer and php-cs-fixer for CI"
+fi
 logk
 
 logn "Installing Mac applications:"
