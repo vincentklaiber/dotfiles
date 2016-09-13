ae150aaa5cc8e7f91db3f93b81a7697c4ed8509a
Vincent Klaiber <vincentklaiber@gmail.com>
2016-09-13 19:43:47 +0200

    Update hyperterm

--- .hyperterm.js
+++ .hyperterm.js
@@ -1,7 +1,7 @@
 module.exports = {
   config: {
     // default font size in pixels for all tabs
-    fontSize: 13,
+    fontSize: '13px',
 
     // font family with optional fallbacks
     fontFamily: 'Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
