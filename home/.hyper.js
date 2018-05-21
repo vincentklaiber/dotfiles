8481bdf5140209378fe0c672109279a440b4455b
Vincent Klaiber <vincentklaiber@gmail.com>
2018-05-21 10:07:18 +0200

    Update tabs

--- home/.hyper.js
+++ home/.hyper.js
@@ -5,7 +5,8 @@ module.exports = {
       .header_header { background-color: #21232d; }
       .tab_tab { color: rgba(155, 159, 181, 0.6); border-color: #181920 !important; background-color: transparent; }
       .tab_tab::before { opacity: 0; }
-      .tab_tab.tab_active { color: #d6d8e1; border-left: 2px solid #5b71f6 !important; background-color: #282a36; }
+      .tab_tab.tab_active { color: #d6d8e1; background-color: #282a36; position: relative; }
+      .tab_tab.tab_active:after { border-left: 2px solid #5b71f6 !important; content: ''; height: 100%; position: absolute; top: 0; }
       .tab_text, .term_term { opacity: 1; }
       .tabs_nav { border-bottom: 1px solid #181920; }
       .tabs_title { display: block !important; }
