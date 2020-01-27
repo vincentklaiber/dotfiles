d90977d7735ab35ab394d32c77497d330cb60e3b
Vincent Klaiber <vincent@doubledip.se>
2020-01-27 20:41:42 +0100

    Fix npm update script
    https://github.com/npm/cli/issues/611

--- home/.config/fish/functions/update.fish
+++ home/.config/fish/functions/update.fish
@@ -13,7 +13,7 @@ function update --description 'update everything'
   composer global update --no-dev --prefer-dist
 
   echo -s (set_color yellow) 'Updating npm...' (set_color normal)
-  npm install --global npm@latest
+  npm install --global --force npm@latest
   npm update --global
 
   echo -s (set_color yellow) 'Removing caches...' (set_color normal)
