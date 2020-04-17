a1005c4a4dceb72b609d21c0fa7331daf1f5e188
Vincent Klaiber <vincent@doubledip.se>
2020-04-17 12:05:44 +0200

    Update update.fish

--- home/.config/fish/functions/update.fish
+++ home/.config/fish/functions/update.fish
@@ -5,7 +5,6 @@ function update --description 'update everything'
   mas upgrade
 
   echo -s (set_color yellow) 'Updating Homebrew...' (set_color normal)
-  brew update
   brew upgrade
 
   echo -s (set_color yellow) 'Updating Composer...' (set_color normal)
@@ -18,7 +17,6 @@ function update --description 'update everything'
 
   echo -s (set_color yellow) 'Removing caches...' (set_color normal)
   rm -rfv (brew --cache)/*
-  brew tap --repair
   brew cleanup
 
   sudo -k
